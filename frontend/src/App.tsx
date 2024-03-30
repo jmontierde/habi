32b47036ee3da15e8c2ce99a32c089381c008921
Jayson Caburatan <montierdejayson@gmail.com>
2024-03-30 17:51:07 +0800

    Setup backend and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([{}]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
