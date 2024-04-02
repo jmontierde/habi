f734025168039fff9817086ca358b6d211adb23c
Jayson Caburatan <montierdejayson@gmail.com>
2024-04-03 00:57:18 +0800

    Added cors

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,17 @@ import user from "./routes/user.js";
 import auth from "./routes/auth.js";
 import post from "./routes/post.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 const app = express();
 
 const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
